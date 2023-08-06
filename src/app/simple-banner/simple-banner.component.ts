7eb794bfc1461fabebc1488e4bf66890d793cb6b
Marcelo Marçal <augustoribeiro.marcal@gmail.com>
2023-08-06 01:09:51 +0100

    Complemento do commit anterior

---
+++ src/app/simple-banner/simple-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-banner',
+  templateUrl: './simple-banner.component.html',
+  styleUrls: ['./simple-banner.component.css']
+})
+export class SimpleBannerComponent {
+  msg = 'Olá, Mundo :)'
+}
