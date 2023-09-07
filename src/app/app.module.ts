d8932f50b026e3fd5367c5ee508493e7615202a6
AUGUMAR <marcelo.augusto@ext.mercedes-benz.io>
2023-09-07 11:34:36 +0100

    Criação do footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SimpleBannerComponent } from './simple-banner/simple-banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SimpleBannerComponent
+    SimpleBannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  isPlaying: boolean = false;
+  currentRadio: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //to-do
+  }
+
+  togglePlayPause() {
+    //to-do
+    this.isPlaying = !this.isPlaying;
+  }
+}
